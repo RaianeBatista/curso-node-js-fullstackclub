c5349db5f84f07b2a3e7af15a484c7d929f94428
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-05-28 15:02:36 -0300

    feat: add file express

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const { Person } = require("./person");
 
 //require("./modules/path");
 //require("./modules/fs")
-require("./modules/http")
-const person = new Person("Raiane");
+//require("./modules/http")
+//const person = new Person("Raiane");
+
+require('./modules/express')
 
 // console.log(person.sayMyName());
