06de0e9ca5ba6f52aafe59b3c2dbdb5295004f82
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-05-28 12:26:29 -0300

    feat: add methods in fs.js module

--- modules/fs.js
+++ modules/fs.js
@@ -11,6 +11,10 @@ const path = require("path");
 // });
 
 // Criar um arquivo
+// Obs: O writeFile é assicrono, não acontece de uma vez
+// 1º O Node fica escrevendo o arquivo, enquanto isso,
+//2.º O Node vai para o appendFile, enquanto isso,
+//3º O Node vai para o readFile, no caso, quando o node le o arquivo, os outros 2 de cima ainda não foram concluidos, especificamente o appendFile, por isso devvemos por todos no mesmo escopo
 fs.writeFile(
   path.join(__dirname, "/test", "test.txt"),
   "hello node!",
@@ -19,6 +23,54 @@ fs.writeFile(
       return console.log("Erro", error);
     }
     console.log("Arquivo criado com sucesso");
+
+    // Adicionar a um arquivo, meto append file
+    fs.appendFile(
+      path.join(__dirname, "/test", "test.txt"),
+      "hello word!",
+      (error) => {
+        if (error) {
+          return console.log("Erro", error);
+        }
+        console.log("Arquivo modificado com sucesso");
+      }
+    );
+
+    // Ler arquivo
+    fs.readFile(
+      path.join(__dirname, "/test", "test.txt"),
+      "utf-8",
+      (error, data) => {
+        if (error) {
+          return console.log("Erro: ", error);
+        }
+        console.log(data);
+      }
+    );
   }
 );
 // Calback - é a função que será executada, quando a escrita do arquivo for finalizado
+
+// Adicionar a um arquivo, meto append file
+// fs.appendFile(
+//   path.join(__dirname, "/test", "test.txt"),
+//   "hello word!",
+//   (error) => {
+//     if (error) {
+//       return console.log("Erro", error);
+//     }
+//     console.log("Arquivo modificado com sucesso");
+//   }
+// );
+
+// // Ler arquivo
+// fs.readFile(
+//   path.join(__dirname, "/test", "test.txt"),
+//   "utf-8",
+//   (error, data) => {
+//     if (error) {
+//       return console.log("Erro: ", error);
+//     }
+//     console.log(data);
+//   }
+// );
