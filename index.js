3a362334da5021f50ad2bbca9c8c3501171d1a6a
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-05-28 17:35:27 -0300

    feat: add database connection

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const { Person } = require("./person");
+//const { Person } = require("./person");
+const dotenv = require("dotenv");
+
+const connectToDatabase = require("./src/database/connect");
+
+dotenv.config();
+
+connectToDatabase();
 
 //require("./modules/path");
 //require("./modules/fs")
 //require("./modules/http")
 //const person = new Person("Raiane");
 
-require('./modules/express')
+//require("./modules/express");
 
 // console.log(person.sayMyName());
--- src/database/connect.js
+++ src/database/connect.js
@@ -1,18 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectToDatabse = async () => {
-  await mongoose.connect(
-    "mongodb+srv://raianebatistasc:<db_password>@cursonodejsfullstackclu.g2lm3rm.mongodb.net/?retryWrites=true&w=majority&appName=CursoNodeJsFullStackClub",
-    (error) => {
-      if (error) {
-        return console.log(
-          "Ocorreu um erro ao se conectar com o banco de dados: ",
-          error
-        );
-      }
-      return console.log("Conexão ao banco de dados realizada com sucesso!");
-    }
-  );
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cursonodejsfullstackclu.g2lm3rm.mongodb.net/database?retryWrites=true&w=majority&appName=CursoNodeJsFullStackClub`
+    );
+    console.log("Conexão ao banco de dados realizada com sucesso!");
+  } catch (error) {
+    console.log("Ocorreu um erro ao se conectar com o banco de dados: ", error);
+  }
 };
 
-module.exports = connectToDatabse
+module.exports = connectToDatabse;
