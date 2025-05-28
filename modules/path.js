f2a874a63c49cb30bb59bf68c9c005fd126b7cd4
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-05-28 12:01:27 -0300

    feat: creating files and folders with the fs module

--- modules/path.js
+++ modules/path.js
@@ -1,13 +1,18 @@
-const path = require('path')
-
-
+const path = require("path");
 
 //Basename
-// Mostra o nome do arquivo atual
-console.log(path.basename(__filename))
+// Mostra o nome do arquivo atual(__filename)
+console.log(path.basename(__filename));
 
 //Nome do Diretório atual
-console.log(path.dirname(__filename))
+console.log(path.dirname(__filename));
 
 // Extensão do arquivo
-console.log(path.extname(__filename))
+console.log(path.extname(__filename));
+
+// Criar objeto Path
+console.log(path.parse(__filename));
+
+// Juntar caminho de arquivos
+console.log(path.join(__dirname, "test", "teste.html"));
+//console.log(__dirname)
